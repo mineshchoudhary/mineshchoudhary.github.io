8b5f11045948eedce978b37fb10770c2abc5fe67
minesh_choudhary <minesh.choudhary@rediffmail.com>
2017-04-02 00:06:43 +0530

    OLD final

--- js/appcontroller.js
+++ js/appcontroller.js
@@ -1,53 +1,42 @@
-var app = angular.module('portfolio',['ngRoute','ui.bootstrap']);
+var app = angular.module('portfolio', ['ngRoute', 'ui.bootstrap']);
 
 app.config(function($routeProvider) {
-	$routeProvider
-	//route for home page
+    $routeProvider
+    //route for home page
 
-	.when('/profile', {
-		templateUrl : 'templates/profile.html',
-		controller  : 'profileController'
-	})
+        .when('/profile', {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileController'
+    })
 
-	.when('/skills', {
-		templateUrl : 'templates/skills.html',
-		controller  : 'skillController'
-	})
+    .when('/skills', {
+            templateUrl: 'templates/skills.html',
+            controller: 'skillController'
+        })
+        .when('/resume', {
+            templateUrl: 'templates/resume.html',
+            controller: 'resumeController'
+        })
 
-	.when('/work', {
-		templateUrl : 'templates/work.html',
-		controller  : 'workController'
-	})
-
-	.when('/resume', {
-		templateUrl : 'templates/resume.html',
-		controller  : 'resumeController'
-	})
-
-	.when('/contact', {
-		templateUrl : 'templates/contact-modal.html',
-		controller  : 'contactController'
-	})
-	
-	.otherwise({redirectTo: '/profile'});
+    .otherwise({ redirectTo: '/profile' });
 })
 
-app.controller('profileController',function($scope) {
-	console.log("-=-=-=profileController-=-=-=");
+app.controller('profileController', function($scope) {
+    console.log("-=-=-=profileController-=-=-=");
 });
 
-app.controller('skillController',function($scope) {
-	console.log("-=-=-=skillController-=-=-=");
+app.controller('skillController', function($scope) {
+    console.log("-=-=-=skillController-=-=-=");
 });
 
-app.controller('workController',function($scope) {
-	console.log("-=-=-=workController-=-=-=");
+app.controller('workController', function($scope) {
+    console.log("-=-=-=workController-=-=-=");
 });
 
-app.controller('resumeController',function($scope) {
-	console.log("-=-=-=resumeController-=-=-=");
+app.controller('resumeController', function($scope) {
+    console.log("-=-=-=resumeController-=-=-=");
 });
 
-app.controller('contactController',function($scope) {
-	console.log("-=-=-=contactController-=-=-=");
+app.controller('contactController', function($scope) {
+    console.log("-=-=-=contactController-=-=-=");
 });
